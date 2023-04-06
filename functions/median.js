1349da94e4582c7a17642f5f4d30b67ae516f3a2
0mgRod <89850217+0mgRod@users.noreply.github.com>
2023-04-06 14:39:20 +0100

    Create median.js

---
+++ functions/median.js
@@ -0,0 +1,8 @@
+function median(numbers) {
+  const sorted = numbers.sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }
+  return sorted[middle];
+}
