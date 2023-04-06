1c2bfaee17599ed435d1a9f8a84c82d0c2773ba7
0mgRod <89850217+0mgRod@users.noreply.github.com>
2023-04-06 14:13:48 +0100

    Update isdeficient.js

--- functions/isdeficient.js
+++ functions/isdeficient.js
@@ -1,4 +1,14 @@
 function isDeficient(number) {
+  function factorize(number) {
+    const factors = [];
+    for (let i = 1; i <= number; i++) {
+      if (number % i === 0) {
+        factors.push(i);
+      }
+    }
+    return factors;
+  }
+
   const factors = factorize(number);
   const sum = factors.reduce((total, factor) => total + factor, 0);
   return sum < number * 2;
